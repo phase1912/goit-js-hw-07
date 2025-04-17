83d7d948e5422d34949598d64b7cc8d814ff126b
Bohdan <phase1912@gmail.com>
2025-04-17 19:35:53 +0200

    added code

--- js/task-3.js
+++ js/task-3.js
@@ -1,25 +1,80 @@
-const profile = {
-    username: "Jacob",
-    playTime: 300,
-    changeUsername: function (newName) {
-        if (newName) {
-            this.username = newName;
+function sortByDescendingFriendCount(users) {
+    return users.toSorted((a, b) => b.friends.length - a.friends.length);
+}
+
+console.log(
+    sortByDescendingFriendCount([
+        {
+            name: "Moore Hensley",
+            friends: ["Sharron Pace"],
+            gender: "male"
+        },
+        {
+            name: "Sharlene Bush",
+            friends: ["Briana Decker", "Sharron Pace"],
+            gender: "female"
+        },
+        {
+            name: "Ross Vazquez",
+            friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+            gender: "male"
+        },
+        {
+            name: "Elma Head",
+            friends: ["Goldie Gentry", "Aisha Tran"],
+            gender: "female"
+        },
+        {
+            name: "Carey Barr",
+            friends: ["Jordan Sampson", "Eddie Strong"],
+            gender: "male"
+        },
+        {
+            name: "Blackburn Dotson",
+            friends: ["Jacklyn Lucas", "Linda Chapman"],
+            gender: "male"
+        },
+        {
+            name: "Sheree Anthony",
+            friends: ["Goldie Gentry", "Briana Decker"],
+            gender: "female"
         }
-    },
-    updatePlayTime: function (hours) {
-        if (hours > 0) {
-            this.playTime += hours;
-        }
-    },
-    getInfo: function () {
-        return `${this.username} has ${this.playTime} active hours!`;
-    }
-};
-
-console.log(profile.getInfo()); // "Jacob has 300 active hours!"
-
-profile.changeUsername("Marco");
-console.log(profile.getInfo()); // "Marco has 300 active hours!"
-
-profile.updatePlayTime(20);
-console.log(profile.getInfo()); // "Marco has 320 active hours!"
+    ])
+);
+// [
+//   {
+//     name: "Ross Vazquez",
+//     friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+//     gender: "male"
+//   },
+//   {
+//     name: "Sharlene Bush",
+//     friends: ["Briana Decker", "Sharron Pace"],
+//     gender: "female"
+//   },
+//   {
+//     name: "Elma Head",
+//     friends: ["Goldie Gentry", "Aisha Tran"],
+//     gender: "female"
+//   },
+//   {
+//     name: "Carey Barr",
+//     friends: ["Jordan Sampson", "Eddie Strong"],
+//     gender: "male"
+//   },
+//   {
+//     name: "Blackburn Dotson",
+//     friends: ["Jacklyn Lucas", "Linda Chapman"],
+//     gender: "male"
+//   },
+//   {
+//     name: "Sheree Anthony",
+//     friends: ["Goldie Gentry", "Briana Decker"],
+//     gender: "female"
+//   },
+//   {
+//     name: "Moore Hensley",
+//     friends: ["Sharron Pace"],
+//     gender: "male"
+//   }
+// ]
